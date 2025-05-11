d86e1be0d8547cd14c3d278e0ec76c8f40f7a251
mando984 <mando984@gmail.com>
2025-05-11 16:01:01 +0200

    Initial commit with Spring application setup

---
+++ src/main/java/com/discord/Projekat_Izbori/DiscordbotApplication.java
@@ -0,0 +1,14 @@
+package com.discord.Projekat_Izbori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordbotApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DiscordbotApplication.class, args);
+	}
+
+}
